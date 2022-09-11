cf73f0e1141e5947de0547de38baa1e93a493fdf
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-09-11 22:18:11 +0300

    Update Javapedia.java

--- Javapedia.java
+++ Javapedia.java
@@ -14,12 +14,24 @@ public class Javapedia {
         for(int i=0; i<database.length; i++){
             System.out.print("\n");
             System.out.print("Figure "+(i+1)+": ");
-            System.out.print("\tName: ");
+            System.out.print("\n\tName: ");
             database[i][0]=input.nextLine();
             System.out.print("\n\tDate of birth: ");
             database[i][1]=input.nextLine();
             System.out.print("\n\tOccupation: ");
             database[i][2]=input.nextLine();
+        }
+        
+        System.out.println("These are the values you stored: ");
+        print2D(database);
+    }
+    
+    public static void print2D(String[][] array){
+        for(int i=0; i<array.length; i++){
+            System.out.println("\nFigure "+(i+1)+": ");
+            for(int j=0; j<array[i].length; j++){
+                System.out.print("\t* "+array[i][j]+"\n");
+            }
         }
     }
 }
